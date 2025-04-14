1e2924c30899d6fbf7fa868d240a66ca8335aa20
ChainSwordCS <chainswordcs@gmail.com>
2025-04-13 23:18:07 -0400

    backport 05a2da8

--- client/Source/main.c
+++ client/Source/main.c
@@ -15,6 +15,7 @@ config cfg;
 Font buttonFont;
 Font fatFont;
 int global = 0;
+Model _controllerModel; // private
 
 int countDigits(int value) {
   if (value < 0)
@@ -29,7 +30,7 @@ int countDigits(int value) {
   return count;
 }
 
-void drawStick(Vector2 pos, HidAnalogStickState stick, HidNpadButton button) {
+void drawStick(Vector2 pos, HidAnalogStickState stick, HidNpadButton button, int materialIndex) {
   Vector2 at;
   at.x = pos.x + stick.x / 800;
   at.y = pos.y + -stick.y / 800;
@@ -42,6 +43,13 @@ void drawStick(Vector2 pos, HidAnalogStickState stick, HidNpadButton button) {
   at.x -= countDigits(global) * 3;
   at.y -= 7;
   DrawTextEx(fatFont, buf, at, 9, 2, WHITE);
+
+  // TODO: test
+  if (materialIndex != -1) {
+    _controllerModel.materials[materialIndex].maps[0].color = (getPacketData()->keys & button) != 0 
+      ? *(Color *)&cfg.colActive
+      : *(Color *)&cfg.colStick;
+  }
 }
 
 Quaternion toQuaternion(float m[3][3]) {
@@ -81,7 +89,7 @@ Quaternion toQuaternion(float m[3][3]) {
 
 void drawButton(Vector2 pos, float width, float height, HidNpadButton button,
                 const char *label, float roundness, float fontOffsetX,
-                float fontOffsetY, float fontSize) {
+                float fontOffsetY, float fontSize, int materialIndex) {
   Rectangle rec;
   rec.x = pos.x - width;
   rec.y = pos.y - height;
@@ -94,15 +102,21 @@ void drawButton(Vector2 pos, float width, float height, HidNpadButton button,
   pos.x += fontOffsetX;
   pos.y += fontOffsetY;
   DrawTextEx(buttonFont, label, pos, fontSize, 4, *(Color *)&cfg.colFont);
+
+  // TODO: test
+  if (materialIndex != -1) {
+    _controllerModel.materials[materialIndex].maps[0].color = (getPacketData()->keys & button) != 0 
+      ? *(Color *)&cfg.colActive
+      : *(Color *)&cfg.colInactive;
+  }
 }
 
 int main() {
   cfg = loadConfig();
-  initSocketShit(cfg);
-  InitWindow(SWIDTH, SHEIGHT, "Horizon-InputDisplay");
-  SetConfigFlags(FLAG_MSAA_4X_HINT);
+  InitWindow(SWIDTH, SHEIGHT, "HOS-InputDisplay");
+  //SetConfigFlags(FLAG_MSAA_4X_HINT);
 
-  buttonFont = LoadFontEx(cfg.fontPath, 38, 0, 250);
+  buttonFont = LoadFontEx(cfg.fontPath, 38, 0, 250); // TODO: try 38, null, 0 ?
   fatFont = LoadFontEx(cfg.fatFontPath, 38, 0, 250);
 
   Camera3D camera = {0};
@@ -117,34 +131,35 @@ int main() {
   camera.position.x = 0;
   camera.position.y = -15;
   camera.position.z = 15;
-  Model cube = LoadModel("Res/Vallarta_Christopher_HW2_Controller.obj");
+  _controllerModel = LoadModel("Res/Vallarta_Christopher_HW2_Controller.obj");
 
-  Vector3 cubePos;
-  cubePos.x = 0;
-  cubePos.y = 0;
-  cubePos.z = 0;
+  Vector3 controllerModelPos;
+  controllerModelPos.x = 0;
+  controllerModelPos.y = 0;
+  controllerModelPos.z = 0;
 
   Quaternion slerpTo;
+  slerpTo.x = 0;
+  slerpTo.y = 0;
+  slerpTo.z = 0;
+  slerpTo.w = 1;
   Quaternion baseRotation;
   baseRotation.x = 0;
   baseRotation.y = 0;
   baseRotation.z = 0;
   baseRotation.w = 1;
-  int cooldown = -1;
+  int cooldown = -1; // calibrationCooldown
+
+  initSocketShit(cfg);
 
   while (1) {
     updateSocketShit();
-
-    BeginDrawing();
-    BeginMode3D(camera);
-
-    ClearBackground(*(Color *)&cfg.colBg);
 
     Quaternion quat = toQuaternion(getPacketData()->state.direction.direction);
     quat.x *= -1;
     quat.y *= -1;
     quat.z *= -1;
-    cube.transform = QuaternionToMatrix(
+    _controllerModel.transform = QuaternionToMatrix(
         QuaternionMultiply(QuaternionInvert(baseRotation), quat));
     if (cooldown > 0) {
       cooldown--;
@@ -161,129 +176,73 @@ int main() {
       slerpTo.y *= -1;
       slerpTo.z *= -1;
     }
-    DrawModel(cube, cubePos, 2, WHITE);
+
+    BeginDrawing();
+    ClearBackground(*(Color *)&cfg.colBg);
+    BeginMode3D(camera);
+    DrawModel(_controllerModel, controllerModelPos, 2, WHITE);
     EndMode3D();
 
     Vector2 pos;
     pos.x = 100;
     pos.y = 180;
-    drawStick(pos, getPacketData()->lPos, HidNpadButton_StickL);
-    cube.materials[11].maps->color =
-        getPacketData()->keys & HidNpadButton_StickL ? *(Color *)&cfg.colActive
-                                                     : *(Color *)&cfg.colStick;
-
+    drawStick(pos, getPacketData()->lPos, HidNpadButton_StickL, 11);
     pos.x = 380;
     pos.y = 270;
-    drawStick(pos, getPacketData()->rPos, HidNpadButton_StickR);
-    cube.materials[12].maps->color =
-        getPacketData()->keys & HidNpadButton_StickR ? *(Color *)&cfg.colActive
-                                                     : *(Color *)&cfg.colStick;
-
+    drawStick(pos, getPacketData()->rPos, HidNpadButton_StickR, 12);
     pos.x = 550;
     pos.y = 180;
-    drawButton(pos, 25, 25, HidNpadButton_A, "A", 1, -12, -19, 38);
-    cube.materials[0].maps->color = getPacketData()->keys & HidNpadButton_A
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 25, 25, HidNpadButton_A, "A", 1, -12, -19, 38, 0);
     pos.x = 450;
     pos.y = 180;
-    drawButton(pos, 25, 25, HidNpadButton_Y, "Y", 1, -12, -19, 38);
-    cube.materials[14].maps->color = getPacketData()->keys & HidNpadButton_Y
-                                         ? *(Color *)&cfg.colActive
-                                         : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 25, 25, HidNpadButton_Y, "Y", 1, -12, -19, 38, 14);
     pos.x = 500;
     pos.y = 230;
-    drawButton(pos, 25, 25, HidNpadButton_B, "B", 1, -12, -19, 38);
-    cube.materials[1].maps->color = getPacketData()->keys & HidNpadButton_B
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 25, 25, HidNpadButton_B, "B", 1, -12, -19, 38, 1);
     pos.x = 500;
     pos.y = 130;
-    drawButton(pos, 25, 25, HidNpadButton_X, "X", 1, -12, -19, 38);
-    cube.materials[13].maps->color = getPacketData()->keys & HidNpadButton_X
-                                         ? *(Color *)&cfg.colActive
-                                         : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 25, 25, HidNpadButton_X, "X", 1, -12, -19, 38, 13);
     pos.x = 380;
     pos.y = 130;
-    drawButton(pos, 12, 12, HidNpadButton_Plus, "+", 1, -9, -19, 38);
-    cube.materials[9].maps->color = getPacketData()->keys & HidNpadButton_Plus
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 12, 12, HidNpadButton_Plus, "+", 1, -9, -19, 38, 9);
     pos.x = 220;
     pos.y = 130;
-    drawButton(pos, 12, 12, HidNpadButton_Minus, "-", 1, -7, -21, 38);
-    cube.materials[8].maps->color = getPacketData()->keys & HidNpadButton_Minus
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 12, 12, HidNpadButton_Minus, "-", 1, -7, -21, 38, 8);
     pos.x = 500;
     pos.y = 70;
-    drawButton(pos, 40, 15, HidNpadButton_R, "R", 1, -12, -20, 38);
-    cube.materials[10].maps->color = getPacketData()->keys & HidNpadButton_R
-                                         ? *(Color *)&cfg.colActive
-                                         : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 40, 15, HidNpadButton_R, "R", 1, -12, -20, 38, 10);
     pos.x = 500;
     pos.y = 30;
-    drawButton(pos, 40, 15, HidNpadButton_ZR, "ZR", 1, -22, -19, 38);
-    cube.materials[16].maps->color = getPacketData()->keys & HidNpadButton_ZR
-                                         ? *(Color *)&cfg.colActive
-                                         : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 40, 15, HidNpadButton_ZR, "ZR", 1, -22, -19, 38, 16);
     pos.x = 100;
     pos.y = 70;
-    drawButton(pos, 40, 15, HidNpadButton_L, "L", 1, -12, -20, 38);
-    cube.materials[7].maps->color = getPacketData()->keys & HidNpadButton_L
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 40, 15, HidNpadButton_L, "L", 1, -12, -20, 38, 7);
     pos.x = 100;
     pos.y = 30;
-    drawButton(pos, 40, 15, HidNpadButton_ZL, "ZL", 1, -22, -19, 38);
-    cube.materials[15].maps->color = getPacketData()->keys & HidNpadButton_ZL
-                                         ? *(Color *)&cfg.colActive
-                                         : *(Color *)&cfg.colInactive;
-
+    drawButton(pos, 40, 15, HidNpadButton_ZL, "ZL", 1, -22, -19, 38, 15);
     pos.x = 220;
     pos.y = 240;
-    drawButton(pos, 15, 15, HidNpadButton_Up, "", .5, 0, 0, 38);
-    cube.materials[6].maps->color = getPacketData()->keys & HidNpadButton_Up
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
+    drawButton(pos, 15, 15, HidNpadButton_Up, "", .5, 0, 0, 38, 6);
     pos.x = 220;
     pos.y = 300;
-    drawButton(pos, 15, 15, HidNpadButton_Down, "", .5, 0, 0, 38);
-    cube.materials[3].maps->color = getPacketData()->keys & HidNpadButton_Down
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
+    drawButton(pos, 15, 15, HidNpadButton_Down, "", .5, 0, 0, 38, 3);
     pos.x = 190;
     pos.y = 270;
-    drawButton(pos, 15, 15, HidNpadButton_Left, "", .5, 0, 0, 38);
-    cube.materials[4].maps->color = getPacketData()->keys & HidNpadButton_Left
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
+    drawButton(pos, 15, 15, HidNpadButton_Left, "", .5, 0, 0, 38, 4);
     pos.x = 250;
     pos.y = 270;
-    drawButton(pos, 15, 15, HidNpadButton_Right, "", .5, 0, 0, 38);
-    cube.materials[5].maps->color = getPacketData()->keys & HidNpadButton_Right
-                                        ? *(Color *)&cfg.colActive
-                                        : *(Color *)&cfg.colInactive;
+    drawButton(pos, 15, 15, HidNpadButton_Right, "", .5, 0, 0, 38, 5);
 
     pos.x = 220;
     pos.y = 270;
     Rectangle rec;
-    rec.x = pos.x - 20;
-    rec.y = pos.y - 15;
+    rec.x = pos.x - 20; // 200
+    rec.y = pos.y - 15; // 255
     rec.height = 15 * 2;
     rec.width = 20 * 2;
     DrawRectangleRounded(rec, 0, 8, *(Color *)&cfg.colInactive);
-    rec.x = pos.x - 15;
-    rec.y = pos.y - 20;
+    rec.x = pos.x - 15; // 205
+    rec.y = pos.y - 20; // 250
     rec.height = 20 * 2;
     rec.width = 15 * 2;
     DrawRectangleRounded(rec, 0, 8, *(Color *)&cfg.colInactive);
